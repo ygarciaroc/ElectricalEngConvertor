5df07ea6b1df78cc9df5a540f46c374330657d2e
WalterQ <149737223+wquinones21@users.noreply.github.com>
2023-11-30 15:38:19 -0700

    Update EEUnitConvertor.c

--- EEUnitConvertor.c
+++ EEUnitConvertor.c
@@ -16,7 +16,7 @@ double a,b;
   scanf("%lf, &a);
   printf(\nEnter the second number");
   scanf("%lf, &a);
-  switch(choice) //Walter
+  switch(choice) 
   {
     case 1:
       printf("%.2lf + %.2lf = %.2lf", a, b, a+b);
@@ -38,4 +38,33 @@ double a,b;
   break;
   }
   return 0;
+}
+//Walter
+#include <stdio.h>
+
+// Function to calculate the voltage across the second resistor in a voltage divider circuit
+double calculateOutputVoltage(double inputVoltage, double resistor1, double resistor2) {
+    if (resistor2 == 0) {
+        printf("Error: Division by zero (The second resistor cannot be zero).\n");
+        return 0;
+    }
+    
+    // Calculating the output voltage using the voltage divider formula
+    double outputVoltage = inputVoltage * (resistor2 / (resistor1 + resistor2));
+    return outputVoltage;
+}
+
+int main() {
+    double sourceVoltage = 12.0; // Input voltage from the source
+    double house1Resistance = 1000.0; // Resistance of the first house
+    double house2Resistance = 2000.0; // Resistance of the second house
+
+    double voltageAcrossHouse2 = calculateOutputVoltage(sourceVoltage, house1Resistance, house2Resistance);
+    
+    printf("Source Voltage: %.2f volts\n", sourceVoltage);
+    printf("Resistance of House 1: %.2f ohms\n", house1Resistance);
+    printf("Resistance of House 2: %.2f ohms\n", house2Resistance);
+    printf("Voltage Across House 2: %.2f volts\n", voltageAcrossHouse2);
+
+    return 0;
 }
